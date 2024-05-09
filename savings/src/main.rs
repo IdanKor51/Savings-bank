bd5a0573026072a3676804393f31f1e4e29f1c69
Idan Kor <idankor06@gmail.com>
2024-05-09 22:44:57 +0300

    template for the menu

--- savings/src/main.rs
+++ savings/src/main.rs
@@ -10,11 +10,24 @@ mod tools;
 fn main(){
     let mut todo_list: Vec<String> = Vec::new();
     'add_to_list: 
+
         loop {
             let out: String = tools::read_inp().unwrap();
-            todo_list.push(out);
+            let _ = match out.as_str(){ 
+                "add" => todo_list.push(String::from("h")),
+                "remove" => {todo_list.pop(); ()},
+                "div" => {
+                    let l = todo_list.len() / 2;
+                    while todo_list.len() > l 
+                    {
+                        todo_list.pop();
+                    };     ()
+                },
+                _ => println!("huh"),
+            };
             if todo_list.len() >10 {break};
             pretty_text::precentage((todo_list.len()*10)as f32);
         }
+
     println!("too many elements");
 }
--- savings/src/tools.rs
+++ savings/src/tools.rs
@@ -2,5 +2,13 @@ pub fn read_inp() -> Result<String, i32>{
     let mut buffer = String::new();
     let stdin = std::io::stdin(); // We get `Stdin` here.
     let _ = stdin.read_line(&mut buffer);
+
+    // get rid of the \n\r at the end of the string
+    let buffer = buffer
+        .chars()
+        .filter(|&x| x != '\n' && x != '\r')
+        .collect();
+    
+    println!("-{}-", buffer);
     Ok(buffer)
 }
